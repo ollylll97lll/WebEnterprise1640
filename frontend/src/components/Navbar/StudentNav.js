e674ab82f7b9aca13cc704e4ab41d1522654c1a9
nhatlam1995 <43503909+nhatlam1995@users.noreply.github.com>
2021-07-25 13:07:59 +0700

    Revert "Nav footer components"

--- frontend/src/components/Navbar/StudentNav.js
+++ frontend/src/components/Navbar/StudentNav.js
@@ -1,36 +1,11 @@
-import React, { useState } from 'react';
-import {
-    Col, Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem,
-    NavLink
-} from 'reactstrap';
-import './index.css';
+import React from 'react'
 
-const StudentNav = (props) => {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggle = () => setIsOpen(!isOpen);
-
+function StudentNav() {
     return (
-        <Navbar light expand="md">
-            <NavbarBrand href="/" >
-                <div className='d-flex align-items-center'>
-                    <div style={{ marginLeft: "-10px" }} />
-                    <div className='ml-2' />
-                    FPT Greenwich Magazine System
-                </div>
-            </NavbarBrand>
-            <NavbarToggler onClick={toggle} />
-            <Collapse isOpen={isOpen} navbar>
-                <Nav className="ml-auto" navbar>
-                    <Col xs="auto">
-                        <NavItem>
-                            <NavLink href='#'>Student</NavLink>
-                        </NavItem>
-                    </Col>
-                </Nav>
-            </Collapse>
-        </Navbar>
+        <div>
+            
+        </div>
     )
-};
+}
 
 export default StudentNav
--- frontend/src/pages/Manager/index.js
+++ frontend/src/pages/Manager/index.js
@@ -1,22 +1,9 @@
 import React from 'react'
-import ManagerNav from '../../components/Navbar/ManagerNav'
-import Timer from '../../components/Timer'
-import Footer from '../../components/Footer'
 
 function ManagerPage() {
     return (
-        <div className="page-container">
-            <div className="content-wrap">
-                <ManagerNav />
-                <br />
-                <div className="col-12">
-                    <Timer />
-                </div>
-                <div className="container">
-                    <div className="text-center">ManagerPage</div>
-                </div>
-            </div>
-            <Footer />
+        <div>
+            
         </div>
     )
 }

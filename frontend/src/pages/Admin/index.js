7961552deb2bea2f150dc791acf43d2040e1edfe
nhatlam1995 <nhatlam1695@gmail.com>
2021-07-25 13:11:20 +0700

    choingu

--- frontend/src/pages/Admin/index.js
+++ frontend/src/pages/Admin/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Footer from '../../components/Footer'
+import AdminNav from '../../components/Navbar/AdminNav'
+import Timer from '../../components/Timer'
 
 function AdminPage() {
     return (
-        <div>
-            
+        <div className="page-container">
+            <div className="content-wrap">
+                <AdminNav />
+                <br />
+                <div className="col-12">
+                    <Timer />
+                </div>
+                <div className="text-center">AdminPage</div>
+            </div>
+            <Footer />
         </div>
     )
 }

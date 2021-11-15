3ab6626567b205c126663f63880428419fb93547
ollylll97lll <58421531+ollylll97lll@users.noreply.github.com>
2021-11-15 12:32:21 +0700

    done upload files 25%

--- frontend/src/pages/MultipleUploadPage/MultipleUp.js
+++ frontend/src/pages/MultipleUploadPage/MultipleUp.js
@@ -4,52 +4,27 @@ import './index.css'
 
 const chunkSize = 10 * 1024; //KB
 export default function MultipleUp() {
-    const [dropZone, setDropZone] = useState(false);
+
+    const [dropzoneActive, setDropzoneActive] = useState(false);
     // tổng files
-    const [files, setFiles] = useState([])
+    const [files, setFiles] = useState([]);
     // vị trí file hiện tại
     const [currentFileIndex, setCurrentFileIndex] = useState(null);
+    const [lastUploadedFileIndex, setLastUploadedFileIndex] = useState(null);
     const [currentChunkIndex, setCurrentChunkIndex] = useState(null);
 
     function handleDrop(e) {
         e.preventDefault();
-        setDropZone(false)
-        if(files.length == 4){
+        setDropzoneActive(false)
+        if (files.length == 4) {
             alert('exceeded maxium files to upload. Please delete previous file and try again')
             return
         }
+        // add files to the array
         setFiles([...files, ...e.dataTransfer.files]);
     }
 
-
-
-    // chỉnh vị trí trong array files để tránh update vào chỗ cũ.
-    useEffect(() => {
-        console.log(files);
-        if (files.length > 0) {
-            // if(currentFileIndex === null){
-            //     setCurrentFileIndex(0)
-            // } 
-            setCurrentFileIndex(currentFileIndex === null ? 0 : currentFileIndex + 1);
-        }
-    }, [files.length])
-
-    // Chọn phần của file
-    useEffect(() => {
-        if (currentFileIndex !== null) {
-            setCurrentChunkIndex(0);
-        }
-    }, [currentFileIndex])
-
-    // upload the chunk
-    useEffect(() => {
-        if (currentChunkIndex !== null) {
-            RnUCurrentChunk();
-
-        }
-    }, [currentChunkIndex])
-
-    function RnUCurrentChunk() {
+    function readAndUploadCurrentChunk() {
         const reader = new FileReader();
         const file = files[currentFileIndex];
         if (!file) {
@@ -58,56 +33,132 @@ export default function MultipleUp() {
         }
         const from = currentChunkIndex * chunkSize;
         const to = from + chunkSize;
-        const dat = file.slice(from, to);
-        reader.onload = (e) => {
-            uploadChunk(e);
-        }
-        reader.readAsDataURL(dat);
+        const blob = file.slice(from, to);
+        reader.onload = e => uploadChunk(e);
+        reader.readAsDataURL(blob);
     }
 
     async function uploadChunk(e) {
-        // const file = files[currentFileIndex];
-        // const data = e.target.result;
-        // const headers = { 'Content-Type': 'application/octet-stream' };
-        // const params = new URLSearchParams();
-        // params.set('name', file.name);
-        // params.set('size', file.size);
-        // params.set('currentChunkIndex', currentChunkIndex);
-        // params.set('totalChunks', Math.ceil(file.size / chunkSize));
+        const file = files[currentFileIndex];
+        const data = e.target.result;
+        const params = new URLSearchParams();
 
-        // const url = `http://localhost:5001/api/upload/uf?${params.toString()}`;
-        // try {
-        //     const response = await axios.post(url, data, headers);
-        //     console.log(response);
-        // } catch (error) {
-        //     console.log(error)
-        // }
+        params.set('name', file.name);
+        params.set('size', file.size);
+        params.set('currentChunkIndex', currentChunkIndex);
+        params.set('totalChunks', Math.ceil(file.size / chunkSize));
+
+        // test set userId & postId;
+        params.set('userId', '614c93f483cb32b7b80f0681');
+        params.set('postId', '6151fdb21788adff1082405a');
+
+        const headers = { 'Content-Type': 'application/octet-stream' };
+
+        const url = `http://localhost:5001/api/upload/fsupload?${params.toString()}`
+        await axios.post(url, data, { headers })
+            .then(response => {
+                const file = files[currentFileIndex];
+                const filesize = files[currentFileIndex].size;
+                // total [chunks - 1] (index of last chunk)
+                const chunks = Math.ceil(filesize / chunkSize) - 1;
+                const isLastChunk = currentChunkIndex === chunks;
+                if (isLastChunk) {
+                    // return file name = final file name in response
+                    file.finalFilename = response.data.finalFilename;
+                    // set previous upload file to this current file index
+                    setLastUploadedFileIndex(currentFileIndex);
+                    // stop uploading chunk by set to null
+                    setCurrentChunkIndex(null);
+                } else {
+                    // continue upload next chunk to temp file
+                    setCurrentChunkIndex(currentChunkIndex + 1);
+                }
+            });
     }
 
+    // chỉnh vị trí trong array files để tránh update vào chỗ cũ.
+    useEffect(() => {
+        // ở file cuối cùng
+        // trả về
+        if (lastUploadedFileIndex === null) {
+            return;
+        }
+        // check file có phải file cuối k
+        const isLastFile = lastUploadedFileIndex === files.length - 1;
+        // set file upload tiếp theo
+        const nextFileIndex = isLastFile ? null : currentFileIndex + 1;
+        setCurrentFileIndex(nextFileIndex);
+
+    }, [lastUploadedFileIndex]);
+
+    useEffect(() => {
+        if (files.length > 0) {
+            if (currentFileIndex === null) {
+                setCurrentFileIndex(
+                    lastUploadedFileIndex === null ? 0 : lastUploadedFileIndex + 1
+                );
+            }
+        }
+    }, [files.length]);
+
+    // Chọn phần của file
+    useEffect(() => {
+        // có file 
+        if (currentFileIndex !== null) {
+            // bắt đầu upload chunk
+            setCurrentChunkIndex(0);
+        }
+    }, [currentFileIndex])
+
+    // upload the chunk
+    useEffect(() => {
+        if (currentChunkIndex !== null) {
+            // update chunk
+            readAndUploadCurrentChunk();
+        }
+    }, [currentChunkIndex])
+
     return (
-        <div style={{ height: '100vh', backgroundColor: '#113', color: '#ddd', padding: '20px' }}>
-            <div
-                onDragOver={e => { setDropZone(true); e.preventDefault(); }}
-                onDragLeave={e => { setDropZone(false); e.preventDefault(); }}
-                onDrop={e => { handleDrop(e) }}
-                className={`dropzone ${dropZone ? 'active' : ''}`}
-            >
-                Drop Your File Here
-            </div>
-            <div
-            // style={{ border: '2px dashed rgba(255,255,255,0.2)', borderRadius: '10px', padding: '50px 0', textAlign: 'center', textTransform: 'uppercase' }}
-            >
-                Display each file here
-                {
-                    files.map(file => {
+        <div>
+            <div style={{ height: '100vh', backgroundColor: '#113', color: '#ddd', padding: '20px' }}>
+                <div
+                    onDragOver={e => { setDropzoneActive(true); e.preventDefault(); }}
+                    onDragLeave={e => { setDropzoneActive(false); e.preventDefault(); }}
+                    onDrop={e => handleDrop(e)}
+                    className={"dropzone" + (dropzoneActive ? " active" : "")}>
+                    Drop your files here
+                </div>
+
+                <div className="files">
+                    {files.map((file, fileIndex) => {
+                        let progress = 0;
+                        // file has final name when its completed
+                        if (file.finalFilename) {
+                            progress = 100;
+                        } else {
+                            // if fileIndex in array equal to 
+                            // current file index then its still uploading
+                            const uploading = fileIndex === currentFileIndex;
+                            // split file size into chunks
+                            const chunks = Math.ceil(file.size / chunkSize);
+                            if (uploading) {
+                                // get progress by using chunk index divided by total chunks
+                                progress = Math.round(currentChunkIndex / chunks * 100);
+                            } else {
+                                progress = 0;
+                            }
+                        }
                         return (
-                            <div>
-                                <span><a href={`http://localhost:5001/api/uploads/${file.filename}` || `../../assets/images/gre.jpg`}>{file.name}</a></span>
-                            </div>
-                        )
-                    })
-                }
+                            <a className="file" target="_blank"
+                                href={'http://localhost:5001/uploads/' + file.finalFilename}>
+                                <div className="name">{file.name}</div>
+                                <div className={"progress " + (progress === 100 ? 'done' : '')}
+                                    style={{ width: progress + '%' }}>{progress}%</div>
+                            </a>
+                        );
+                    })}
+                </div>
             </div>
         </div>
-    )
+    );
 }
--- server/newroutes/uploading.js
+++ server/newroutes/uploading.js
@@ -8,6 +8,8 @@ const File = require('../newmodels/Files');
 const path = require('path');
 const md5 = require('md5');
 
+const uploadfolderName = './uploads'
+
 
 // local upload
 const storage = multer.diskStorage({
@@ -91,6 +93,76 @@ router.post('/gul', async (req, res) => {
     const result = await File.findOne({ postId: postId }).sort({ createdAt: 'desc' });
     res.status(200).send(result)
 })
+
+
+// NO MULTER UPLOAD
+
+router.post('/fsupload', (req, res) => {
+    const { name, currentChunkIndex, totalChunks } = req.query;
+    const { userId, postId } = req.query;
+    // post folder name
+    const POST_FOLDER_NAME = md5(userId, postId);
+
+    // get chunk position is at first chunk ?
+    const firstChunk = parseInt(currentChunkIndex) === 0;
+    // get chunk position is at last chunk
+    const lastChunk = parseInt(currentChunkIndex) === parseInt(totalChunks) - 1;
+
+    // get file ext name
+    const ext = name.split('.').pop();
+    // data e.x: [34, 24, 52] (array buffer sent into small parts)
+    const data = req.body.toString().split(',')[1];
+    // convert data to buffer 
+    const buffer = Buffer.from(data, 'base64')
+    // new Buffer(data, 'base64');
+    // temporary file name
+    const tmpFilename = 'tmp_' + md5(name + req.ip) + '.' + ext;
+
+    // check uploads folder exist
+    if (!fs.existsSync(uploadfolderName)) {
+        // if not make new folder with that name
+        try {
+            fs.mkdirSync(uploadfolderName, { recursive: true })
+            return res.send({ success: false, message: 'Root Folder Uploads was just created. Pls re upload your file' })
+        } catch (err) {
+            if (err.code !== 'EEXIST') throw err
+            res.send({ success: false, err });
+        }
+    }
+
+    // check the post upload folder exist
+    if (!fs.existsSync(uploadfolderName + '/' + POST_FOLDER_NAME)) {
+        // create new
+        try {
+            fs.mkdirSync(uploadfolderName + '/' + POST_FOLDER_NAME, { recursive: true })
+        } catch (error) {
+            if (err.code !== 'EEXIST') throw err
+            res.send({ success: false, err });
+        }
+    }
+
+
+    //if the chunk is th first chunk and there is the same temp file
+    // as the file was about to upload
+    if (firstChunk && fs.existsSync(uploadfolderName + '/' + POST_FOLDER_NAME + '/' + tmpFilename)) {
+        // remove that file
+        fs.unlinkSync(uploadfolderName + '/' + POST_FOLDER_NAME + '/' + tmpFilename);
+    }
+    // else upload that file
+    fs.appendFileSync(uploadfolderName + '/' + POST_FOLDER_NAME + '/' + tmpFilename, buffer);
+    // if is the last chunk
+    if (lastChunk) {
+        // generate final name
+        const finalFilename = md5(Date.now()).substr(0, 6) + '.' + ext;
+        // change the temp file to final file
+        fs.renameSync(uploadfolderName + '/' + POST_FOLDER_NAME + '/' + tmpFilename, uploadfolderName + '/' + POST_FOLDER_NAME + '/' + finalFilename);
+        // send final file name
+        res.json({ finalFilename });
+    } else {
+        // if still uploading
+        res.json('uploading');
+    }
+});
 
 
 module.exports = router
